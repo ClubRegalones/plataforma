c07e266ff4817d0f0822d451186bd84014e8b48e
Bárbara Jopia Castro <barbarajopia98@gmail.com>
2026-09-07 22:35:14 -0300

    style: activa marca PNG alta calidad en App Negocio

--- apps/negocio/src/main.tsx
+++ apps/negocio/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Raiz from './Raiz'
 import './index.css'
-import './marca-svg.css'
+import './marca-png.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
