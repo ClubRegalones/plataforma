3803fc8e4166079b597b19b3889b7ad23b997962
Bárbara Jopia Castro <barbarajopia98@gmail.com>
2026-09-07 22:53:57 -0300

    feat: inicializa tema persistente App Negocio

--- apps/negocio/src/main.tsx
+++ apps/negocio/src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Raiz from './Raiz'
+import { inicializarTema } from './lib/tema'
 import './index.css'
 import './marca-png.css'
+import './tema.css'
+
+inicializarTema()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
