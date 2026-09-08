faa45f17683ec9b1eb6b6cb7ef4a8664abbdef0a
Bárbara Jopia Castro <barbarajopia98@gmail.com>
2026-09-07 22:27:50 -0300

    style: carga activos SVG oficiales en App Negocio

--- apps/negocio/src/main.tsx
+++ apps/negocio/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Raiz from './Raiz'
 import './index.css'
+import './marca-svg.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
